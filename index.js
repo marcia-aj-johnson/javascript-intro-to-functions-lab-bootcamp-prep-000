2cf95b7003fa89380e8e3a40c3fc22272ab5a3e6
Marcia Johnson <marcia.johnson87@gmail.com>
2018-04-08 06:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === whisper(string)) 
     return "I can't hear you!"
-
-}else if(string === shout(string)){
+  else if(string === shout(string))
     return "YES INDEED!"
-    
-}else if(string === "I love you, Grandma."){
-  return "Ilove you too."
+  else if(string === "I love you, Grandma.")
+    return "I love you too."
 }
